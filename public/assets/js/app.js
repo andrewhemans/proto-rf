a8a121ca470b60ba16fb8771a85168a8e2b65bd9
Andrew Hemans <andrewhemans@Andrews-MacBook-Pro.local>
2018-06-26 00:25:26 -0700

    spreadsheet page and some js functionality

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,11 @@
+
+var editor = ace.edit("editor");
+var modalTitle;
+var modalExpect;
+var modalNotes;
+var modalSteps;
+
+
 $("#showModal").click(function() {
   $(".modal").addClass("is-active");
 });
@@ -9,3 +17,42 @@ $(".delete").click(function() {
 $("#closeModal").click(function() {
    $(".modal").removeClass("is-active");
 });
+
+
+// clears form field
+$( "#row-1" ).click(function() {
+  $('.row-1').val('');
+  console.log('you clicked');
+});
+
+
+
+
+$( "#addTests" ).click(function() {
+$( ".spreadsheet" ).append( '<tr><td class="sheet-cell"><input class="input sheet-input" type="text" placeholder=""></td><td class="sheet-cell"><input class="input sheet-input" type="text" placeholder=""></td><td class="sheet-cell"><input class="input sheet-input" type="text" placeholder=""></td><td class="sheet-cell"><input class="input sheet-input" type="text" placeholder=""></td><td class="sheet-cell"><input class="input sheet-input" type="text" placeholder=""></td><td class="remove-row sheet-remove" width="36px"><i class="far fa-trash-alt"></i></td></tr>' );
+console.log('you clicked');
+});
+
+
+// modal form
+$( "#modalNew" ).click(function() {
+  console.log('modal click');
+  modalTitle = $('#modalTitle').val();
+  modalExpect = $('#modalExpect').val();
+  modalNotes = $('#modalNotes').val();
+  modalSteps = editor.getValue();
+  console.log(modalSteps);
+  addTestToTable();
+});
+
+function addTestToTable() {
+  $( ".table-body" ).append("<tr></tr>");
+
+  $( ".table-body:last-child" ).append("<td>" + modalTitle + "</td>");
+  $( ".table-body:last-child" ).append("<td>5 Steps</td>");
+  $( ".table-body:last-child" ).append("<td>" + modalExpect + "</td>");
+  $( ".table-body:last-child" ).append('<td class="remove-row"><i class="far fa-trash-alt"></i></td>');
+
+  console.log("add test function");
+
+};
